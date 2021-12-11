58f85af9b6f07ff69bb155fe1750e72d25a3d2d9
JT <dd3557@naver.com>
2021-12-12 02:50:20 +0900

    style: ExpoInformationViewController의 개행 코드 수정

--- Expo1900/Expo1900/Controller/ExpoInformationViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoInformationViewController.swift
@@ -7,12 +7,9 @@
 import UIKit
 
 class ExpoInformationViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
