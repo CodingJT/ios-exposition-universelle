39c2d6bbcbba27506d33c19262e1d8d3a1a657fa
JT <dd3557@naver.com>
2021-12-12 04:44:21 +0900

    file: ExpoInformationTableViewCell 클래스 파일 생성

---
+++ Expo1900/Expo1900/View/ExpoInformationTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ExpoInformationTableViewCell.swift
+//  Expo1900
+//
+//  Created by 김진태 on 2021/12/12.
+//
+
+import UIKit
+
+class ExpoInformationTableViewCell: UITableViewCell {
+    static let identifier = String(describing: self)
+}
