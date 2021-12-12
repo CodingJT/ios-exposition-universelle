362c9df18dec7cdde8a19a3282e0afa0756d8915
JT <dd3557@naver.com>
2021-12-12 16:08:34 +0900

    file: ExhibitionItemTableViewCell 클래스 파일 추가

---
+++ Expo1900/Expo1900/View/ExhibitionItemTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ExhibitionItemTableViewCell.swift
+//  Expo1900
+//
+//  Created by 김진태 on 2021/12/12.
+//
+
+import UIKit
+
+class ExhibitionItemTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.numberOfLines = 0
+        }
+    }
+    @IBOutlet weak var descriptionLabel: UILabel! {
+        didSet {
+            descriptionLabel.numberOfLines = 0
+        }
+    }
+    @IBOutlet weak var itemImageView: UIImageView!
+}
