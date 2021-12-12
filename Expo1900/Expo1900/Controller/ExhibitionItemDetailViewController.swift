108fddef3b190fecf1216af4f3e6282b7d2df002
JT <dd3557@naver.com>
2021-12-12 16:55:07 +0900

    feat: ExhibitionItemDetailViewController 코드와 비어있는 Storyboard Scene 연결

---
+++ Expo1900/Expo1900/Controller/ExhibitionItemDetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ExhibitionItemDetailViewController.swift
+//  Expo1900
+//
+//  Created by 김진태 on 2021/12/12.
+//
+
+import UIKit
+
+class ExhibitionItemDetailViewController: UIViewController {
+    
+}
