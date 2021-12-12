977ef5282f4231fdb12971a9623bbd64b772fbe7
JT <dd3557@naver.com>
2021-12-12 16:20:31 +0900

    feat: ExhibitionItemsViewController에서 로컬 JSON을 읽어오는 코드 추가

--- Expo1900/Expo1900/Controller/ExhibitionItemsViewController.swift
+++ Expo1900/Expo1900/Controller/ExhibitionItemsViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 class ExhibitionItemsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
+    var exhibitionItems: [ExhibitionItem]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadExhibitionItemsData()
+    }
+    
+    func loadExhibitionItemsData() {
+        let jsonDecoder = JSONDecoder()
+        guard let exhibitionItemsData = NSDataAsset(name: "items") else { return }
+        
+        do {
+            let decodedExhibitionItems = try jsonDecoder.decode([ExhibitionItem].self, from: exhibitionItemsData.data)
+            self.exhibitionItems = decodedExhibitionItems
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
